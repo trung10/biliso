5c74873bcfd34a083bd125909cc4d36f35118833
Pham Duc Trung <>
2022-02-25 10:54:51 +0700

    Initial commit

---
+++ app/src/main/java/com/trungpd/biliso/MainActivity.kt
@@ -0,0 +1,11 @@
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trungpd.biliso.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
